f9b0ca10fef61f80233b5368a1886f787e89a863
Ekaterina Alekseeva <Ekaterina.Alekseeva@jetbrains.com>
2015-07-21 10:10:46 +0300

    ugly workaround removed

--- src/Parser.java
+++ src/Parser.java
@@ -290,7 +290,7 @@ public class Parser {
                 if (ignored && !ignoredSelectors.contains(parparSelector + " " + parSelector + " " + cssSelector)) {
                     ignoredSelectors.add(parparSelector + " " + parSelector + " " + cssSelector);
                 } else
-                    if (!ignored && !ignoredSelectors.contains(parparSelector + " " + parSelector + " " + cssSelector) && !selectors.contains(parparSelector + " " + parSelector + " " + cssSelector) && !(parparSelector + " " + parSelector + " " + cssSelector).equals("div[class*='yt-attach-file-dialog__permitted-group-fieldset'] div[class*='combobox'] a[class*='arrow']")) {
+                    if (!ignored && !ignoredSelectors.contains(parparSelector + " " + parSelector + " " + cssSelector) && !selectors.contains(parparSelector + " " + parSelector + " " + cssSelector)) {
                         Element tmpEl = new Element();
                         tmpEl.setElement(elem);
                         tmpEl.setTerminal(terminal);
